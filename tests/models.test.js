da10efdf919f79f5eb45d0af84216be00580cedf
nicolasarths <nicolasarths@hotmail.com>
2022-06-01 19:53:50 -0300

    tests folder created, tests for controllers created and working

---
+++ tests/models.test.js
@@ -0,0 +1,17 @@
+const Sheets = require ('../models')
+const { spreadsheetId } = require ('../config')
+
+describe('Connecting to google sheets', () => {
+  test('Variable spreadsheetId succesfully defined at config/connection.js', () => {
+    expect(typeof(spreadsheetId)).toBe('string')
+  })
+
+  test('Succesfully connects to a spreadsheet / and this spreadsheet\'s title is accessible', async () => {
+    const data = await Sheets.data()
+    let isConnected = false
+    if (data.properties.title) {
+      isSpreadsheets = true
+    }
+    expect(isSpreadsheets).toBeTruthy()
+  })
+})
