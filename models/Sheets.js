6658f09341c4fb9c14d8cf71bcaebce1818f9de6
nicolasarths <nicolasarths@hotmail.com>
2022-06-01 18:49:22 -0300

    first tests working on models

--- models/Sheets.js
+++ models/Sheets.js
@@ -1,6 +1,13 @@
 const { googleSheetsAPI, auth, spreadsheetId } = require('../config')
 
+//Define class Sheets to gather info from the spreadsheet file
 class Sheets {
+    //Get main data from the spreadsheet file, using the function that comes after
+    static async data () {
+        const metaData = await this.metaData()
+        return metaData.data;
+    }
+    //Get the metaData from the file, lots of data
     static async metaData () {
         const api = await googleSheetsAPI();
         const metaData = await api.spreadsheets.get({
@@ -9,12 +16,6 @@ class Sheets {
         })
         return metaData
     }
-    static async data () {
-        const metaData = await this.metaData()
-        return metaData.data;
-    }
 }
-
-Sheets.data().then(a => console.log(a.properties.title))
 
 module.exports = Sheets;
--- models/index.test.js
+++ models/index.test.js
@@ -1,13 +1,12 @@
 const { Sheets, Cells } = require ('../models')
 const { spreadsheetId } = require ('../config')
-// {}
 
 describe('Connecting to google sheets', () => {
-  test('Variable spreadsheet ID succesfully defined', () => {
+  test('Variable spreadsheetId succesfully defined at config/connection.js', () => {
     expect(typeof(spreadsheetId)).toBe('string')
   })
 
-  test('Succesfully connects to a spreadsheet', async () => {
+  test('Succesfully connects to a spreadsheet / and this spreadsheet\'s title is accessible', async () => {
     const data = await Sheets.data()
     let isConnected = false
     if (data.properties.title) {
