eac7766f51811c2be60277c268a4112090b14fda
nicolasarths <nicolasarths@hotmail.com>
2022-06-01 21:21:25 -0300

    routes index now works as router, redundant files deleted

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
+//Currently using bodyParser because I learned routing with it,
+//Intend to study how it works better so that I can decide if I really need it or not
+//Also need to learn how to use routes without it! - N.A. June, 2022.
 const bodyParser = require('body-parser')
-const sheets = require('./sheetRouter')
+const { Router } = require('express')
+const { SheetsController } = require('../controllers/')
 
+const router = Router()
+
+router
+  //A route to access main data from the spreadsheet
+  .get('//spreadsheet', SheetsController.readData)
+  //A route to access lots of meta data of said spreadsheet 
+  .get('/spreadsheet/meta', SheetsController.readMetaData)
+
+//Export app to use it on the server.
 module.exports = app => {
   app.use(
     bodyParser.json(),
-    sheets
+    router
   )
 }
