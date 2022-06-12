c06973ae2bdfc63c0a1271b40453154ae4e154d6
nicolasarths <nicolasarths@hotmail.com>
2022-06-12 19:36:04 -0300

    fixing bugs due to api's folder being created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const { app: router } = require('./routes');
+const { app: router } = require('./api/routes');
 const port = 3000;
 
 router(app);
