9a1e959065484819e350aae2f64d67f1a3dbd5c4
nicolasarths <nicolasarths@hotmail.com>
2022-06-12 21:16:50 -0300

    sending api related files to api folder

---
+++ api/spreadsheetId.js
@@ -0,0 +1,1 @@
+module.exports = '1q3pU2CE34m1IvilyN-wN9TO52Ob687anPe-D2PaV21U';
---
+++ api/tests/controllers.test.js
@@ -0,0 +1,20 @@
+const { mockRequest, mockResponse } = require('./util/interceptor')
+const { SheetsController, data } = require('../controllers')
+
+describe("Check method \'readData\' ", () => {
+    test('data is being retreived from models', async () => {
+        const retreivedData = await data;
+        const dataProof = retreivedData.properties.title
+        expect(typeof(dataProof)).toBe('string')
+    })
+    
+    test('readData returns status 200', async () => {
+        let req = mockRequest();
+        req.params.id = 1;
+        const res = mockResponse();
+
+        await SheetsController.readData(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(200);
+    });
+});
